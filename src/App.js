c24f64ea07a1c21e02851e061f34f6def40ea5de
SRoller <sroller@justfab.com>
2018-05-29 17:22:07 -0400

    adds firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
 import reducers from './reducers';
 import firebaseconfig from './config/firebaseconfig'
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
     componentDidMount() {
@@ -19,13 +20,11 @@ class App extends Component {
     }
 
     render() {
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
         return (
-            <Provider store={createStore(reducers)}>
-                <View>
-                    <Text>
-                        Hello!
-                    </Text>
-                </View>
+            <Provider store={store}>
+                <LoginForm />
             </Provider>
         );
     }
