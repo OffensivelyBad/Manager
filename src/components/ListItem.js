764f2a40c6f1160ca299f3469f47f858ac169d75
SRoller <sroller@justfab.com>
2018-06-19 21:28:53 -0400

    fixes passing in employee data

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback } from 'react-native';
+import { Text, TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { CardSection } from './common';
 
 class ListItem extends Component {
 
     onRowPress() {
-        Actions.employeeCreate();
+        Actions.employeeCreate({ employee: this.props.employee });
     }
 
     render() {
         const { name } = this.props.employee;
 
         return (
-            <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+            <TouchableOpacity onPress={this.onRowPress.bind(this)}>
                 <CardSection>
                     <Text style={styles.titleStyle}>
                         {name}
                     </Text>
                 </CardSection>
-            </TouchableWithoutFeedback>
+            </TouchableOpacity>
         );
     }
 }
